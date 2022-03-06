51145870a1729d9080baa12afd02e140bfa0e135
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2022-03-06 23:20:30 +0400

    tablet - home page and categories

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,51 @@
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
+const cart = document.getElementById("cart");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+if (cart) {
+  cart.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    modal.classList.add("shown");
+  });
+  document.addEventListener("click", function (event) {
+    if (!event.target.closest("#cart")) {
+      if (!event.target.closest("#modal-content")) {
+        modal.classList.remove("shown");
+        modal.classList.add("hidden");
+      }
+    }
+  });
 }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
 // payment method radio button
 //TODO
+
+// checkout modal
+
+const checkoutButton = document.getElementById("checkoutButton");
+const checkoutModal = document.getElementById("checkout-modal");
+const checkoutContainer = document.getElementById("checkout-container");
+if (checkoutButton) {
+  checkoutButton.addEventListener("click", () => {
+    checkoutModal.classList.remove("hidden");
+    const otherItems = document.getElementById("other-items");
+    otherItems.addEventListener("click", () => {
+      const temp = otherItems.innerText;
+      otherItems.previousElementSibling.classList.remove("hidden");
+      otherItems.previousElementSibling.previousElementSibling.classList.remove(
+        "hidden"
+      );
+      otherItems.classList.add("hidden");
+      const p = document.createElement("p");
+      p.innerText = "View less";
+      p.className = "temp";
+      otherItems.parentElement.appendChild(p);
+      p.addEventListener("click", () => {
+        otherItems.previousElementSibling.classList.add("hidden");
+        otherItems.previousElementSibling.previousElementSibling.classList.add(
+          "hidden"
+        );
+        p.remove();
+        otherItems.classList.remove("hidden");
+      });
+    });
+  });
+}
